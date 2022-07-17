3a98638a442bc48368139d0c06508fcf5ff3e3ef
byeolyee <byeolyee2230@gmail.com>
2022-07-17 19:19:13 +0900

    footer

--- js/index.js
+++ js/index.js
@@ -21,10 +21,3 @@ function initMap() {
 }
 
 initMap();
-
-// const container = document.getElementById('map');
-// const options = {
-//     center: new kakao.maps.LatLng(33.450701, 126.570667),
-//     level: 3
-// };
-// const map = new kakao.maps.Map(container, options);
